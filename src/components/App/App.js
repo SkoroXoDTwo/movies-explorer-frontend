90cb61abfef17d534e4a0534e3359da15a5e365a
Alexey Belkov <lexabelco2013@yandex.ru>
2023-04-26 23:59:44 +0500

    feat: добавить роуты

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Main from '../Main/Main';
 
 function App() {
   return (
-    <></>
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/movies" element={<Movies />} />
+      <Route path="/saved-movies" element={<SavedMovies />} />
+      <Route path="/profile" element={<Register />} />
+      <Route path="/signin" element={<Login />} />
+      <Route path="/signup" element={<Profile />} />
+    </Routes>
   );
 }
 
