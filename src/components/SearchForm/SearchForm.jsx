c9b008e2b95f64261d06ac701679ca591b6d036a
Alexey Belkov <lexabelco2013@yandex.ru>
2023-05-20 22:09:16 +0500

    feat: реализовать работу toggle

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,16 +1,22 @@
+import { useState } from 'react';
 import './SearchForm.css';
 
 function SearchForm() {
+  const [isActiveToggleBtn, setIsActiveToggleBtn] = useState(false)
+
   return (
     <section className='search-form'>
       <div className='search-form__container'>
         <form className='search-form__input-container'>
           <div className='search-form__input-icon' />
-          <input className='search-form__input' placeholder='Фильм'/>
+          <input className='search-form__input' placeholder='Фильм' />
           <button className='search-form__input-btn' />
         </form>
         <div className='search-form__toggle-container'>
-          <button className='search-form__toggle-btn' />
+          <button
+            className={`search-form__toggle-btn ${isActiveToggleBtn ? 'search-form__toggle-btn_active' : ''}`}
+            onClick={() => setIsActiveToggleBtn(!isActiveToggleBtn)}
+          />
           <p className='search-form__toggle-text'>Короткометражки</p>
         </div>
       </div>
