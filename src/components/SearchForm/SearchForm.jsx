122b5c2a649d83a3a992f0c436f60e518f88b3eb
Alexey Belkov <lexabelco2013@yandex.ru>
2023-05-21 21:14:23 +0500

    feat: адаптировать SearchForm

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -6,20 +6,24 @@ function SearchForm() {
 
   return (
     <section className='search-form'>
-      <div className='search-form__container'>
-        <form className='search-form__input-container'>
-          <div className='search-form__input-icon' />
-          <input className='search-form__input' placeholder='Фильм' />
-          <button className='search-form__input-btn' />
-        </form>
-        <div className='search-form__toggle-container'>
-          <button
-            className={`search-form__toggle-btn ${isActiveToggleBtn ? 'search-form__toggle-btn_active' : ''}`}
-            onClick={() => setIsActiveToggleBtn(!isActiveToggleBtn)}
-          />
-          <p className='search-form__toggle-text'>Короткометражки</p>
+      <form className='search-form__container' onSubmit={(e) => e.preventDefault()}>
+        <div className='search-form__row'>
+          <div className='search-form__input-container'>
+            <div className='search-form__input-row'>
+              <div className='search-form__input-icon' />
+              <input className='search-form__input' placeholder='Фильм' />
+              <button className='search-form__input-btn' />
+            </div>
+          </div>
+          <div className='search-form__toggle-container'>
+            <button
+              className={`search-form__toggle-btn ${isActiveToggleBtn ? 'search-form__toggle-btn_active' : ''}`}
+              onClick={() => setIsActiveToggleBtn(!isActiveToggleBtn)}
+            />
+            <p className='search-form__toggle-text'>Короткометражки</p>
+          </div>
         </div>
-      </div>
+      </form>
     </section>
   );
 }
