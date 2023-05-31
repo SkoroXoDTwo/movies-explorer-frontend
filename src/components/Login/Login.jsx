577ae197ba7b3868de47495a5471a6a8dc4c80c3
unknown <belkov.aleksey@bodysite.ru>
2023-05-31 17:08:12 +0500

    feat: сверстать Login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,15 +1,43 @@
+import { Link } from 'react-router-dom';
 import './Login.css';
 
 function Login() {
   return (
     <div className='login'>
       <header className='login__header'>
-        <a className='login__header-logo' href='#' />
+        <Link className='login__header-logo' to='/' />
       </header>
       <main className='login__main'>
         <h1 className='login__title'>
-          Добро пожаловать!
+          Рады видеть!
         </h1>
+        <div className='login__container'>
+          <div className='login__inputs'>
+            <div className='login__input-container'>
+              <p className='login__input-title'>
+                E-mail
+              </p>
+              <input className='login__input' type="email" required />
+              <p className='login__input-error'>Что-то пошло не так...</p>
+            </div>
+            <div className='login__input-container'>
+              <p className='login__input-title'>
+                Пароль
+              </p>
+              <input className='login__input' type='password' required />
+              <p className='login__input-error login__input-error_visible'>Что-то пошло не так...</p>
+            </div>
+          </div>
+          <div className='login__btns'>
+            <button className='login__btn-auth'>Войти</button>
+            <div className='login__footer-link-container'>
+              <p className='login__footer-link-text'>
+                Ещё не зарегистрированы?
+              </p>
+              <Link to="signup" className='login__footer-link'>Регистрация</Link>
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   );
