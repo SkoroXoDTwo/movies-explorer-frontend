a7feee418485eec6e93a8717c12b9e05446079bb
unknown <belkov.aleksey@bodysite.ru>
2023-06-05 16:49:16 +0500

    feat: защитить роуты

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,17 +7,47 @@ import Register from '../Register/Register';
 import Login from '../Login/Login';
 import Profile from '../Profile/Profile';
 import PageNotFound from '../PageNotFound/PageNotFound';
+import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div className="page">
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/saved-movies" element={<SavedMovies />} />
-        <Route path="/profile" element={<Profile />} />
         <Route path="/signin" element={<Login />} />
         <Route path="/signup" element={<Register />} />
+
+        <Route
+          path="/movies"
+          element={
+            <ProtectedRoute
+              Component={<Movies />}
+              isLoggedIn={isLoggedIn}
+            />
+          }
+        />
+        <Route
+          path="/saved-movies"
+          element={
+            <ProtectedRoute
+              Component={<SavedMovies />}
+              isLoggedIn={isLoggedIn}
+            />
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute
+              Component={<Profile />}
+              isLoggedIn={isLoggedIn}
+            />
+          }
+        />
+
         <Route path="/*" element={<PageNotFound />} />
       </Routes>
     </div>
